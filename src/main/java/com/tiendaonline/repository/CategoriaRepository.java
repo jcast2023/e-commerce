5aa6dae99a524486eb7129a12d75c64be3e5db2b
jcastili <jul_ed@hotmail.com>
2026-05-31 17:19:03 -0500

    Revert "primer commit - backend marianistore"
    This reverts commit 0fd904ddcd71e4f39841689f28fd391e9a3335f6.

--- src/main/java/com/tiendaonline/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiendaonline.repository;
-
-import com.tiendaonline.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
--- src/main/java/com/tiendaonline/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.tiendaonline.service;
-
-import com.tiendaonline.model.Reclamo;
-
-public interface EmailService {
-    void enviarCorreoConfirmacion(String emailDestino, String nombreCliente, Object idPedido, Double totalPedido);
-    void enviarCorreoReclamo(Reclamo reclamo);
-}
